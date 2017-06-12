61f9a6820c2bcfb1fc440f4e9453c58579a7c667
Konstantine Zheltov <brandaser2@gmail.com>
2017-06-12 21:27:23 +0300

    fixed typo

--- app/config/index.js
+++ app/config/index.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
         dbURI: process.env.dbURI,
         sessionSecret: process.env.sessionSecret,
         fb: {
-            cliendID: process.env.fbClientID,
+            clientID: process.env.fbClientID,
             clientSecret: process.env.fbClientSecret,
             callbackURL: process.env.host + "/auth/facebook/callback",
             profileFields: ['id',  'displayName', 'photos']
